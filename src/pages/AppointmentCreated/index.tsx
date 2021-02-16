9629be4435b88d4d0dce80fc2ba107f8c9b96edb
Alcir Junior Caju <junior@cajucomunica.com.br>
2021-02-16 07:28:41 -0300

    Add Header Component in Pages

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Header from '@components/Header';
+
 import { Container } from './styles';
 
 const AppointmentCreated: React.FC = () => {
   return (
     <Container>
-      <h1>AppointmentCreated</h1>
+      <Header />
     </Container>
   );
 };
--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Header from '@components/Header';
+
 import { Container } from './styles';
 
 const CreateAppointment: React.FC = () => {
   return (
     <Container>
-      <h1>CreateAppointment</h1>
+      <Header />
     </Container>
   );
 };
