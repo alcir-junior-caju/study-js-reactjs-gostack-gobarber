f5f4a538f5075d54af60514600864caa6a4fda24
Alcir Junior Caju <junior@cajucomunica.com.br>
2021-02-15 21:47:35 -0300

    Creating the Appointments Create structure

---
+++ src/pages/CreateAppointment/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const CreateAppointment: React.FC = () => {
+  return (
+    <Container>
+      <h1>CreateAppointment</h1>
+    </Container>
+  );
+};
+
+export default CreateAppointment;
