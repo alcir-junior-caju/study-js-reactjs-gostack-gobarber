a6233581122c3d1a23c709b5223b7b5102c8c4be
Alcir Junior Caju <junior@cajucomunica.com.br>
2021-02-20 13:58:53 -0300

    Add DatePicker Hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import { AuthProvider } from '@hooks/auth';
+import { DatePickerProvider } from '@hooks/datePicker';
 import { ToastProvider } from '@hooks/toast';
 
 const AppProvider: React.FC = ({ children }) => {
   return (
     <AuthProvider>
-      <ToastProvider>{children}</ToastProvider>
+      <DatePickerProvider>
+        <ToastProvider>{children}</ToastProvider>
+      </DatePickerProvider>
     </AuthProvider>
   );
 };
